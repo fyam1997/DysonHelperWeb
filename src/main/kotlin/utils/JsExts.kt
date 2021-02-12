0e198d920ae9221c0f0bbca532725f3959a9aa80
Fyam <fung19971115@gmail.com>
2021-02-12 20:44:02 +0800

    - Update getJsonArray

--- src/main/kotlin/utils/JsExts.kt
+++ src/main/kotlin/utils/JsExts.kt
@@ -14,14 +14,32 @@ val Json.keys get() = jsObject.keys(this).unsafeCast<Array<String>>()
 
 fun <T> Json.map(parser: (item: dynamic) -> T) = keys.map { parser(get(it)) }
 
+fun <T> Json.mapList(parser: (item: dynamic) -> T) = this.asDynamic()
+
 fun <T> Json.toMap(parser: (item: dynamic) -> T) = keys.map { it to parser(get(it)) }.toMap()
 
 val JQueryXHR.json get() = responseJSON.asDynamic().unsafeCast<Json>()
 
-suspend fun JQueryStatic.getJsonSuspend(url: String): Json {
+suspend fun JQueryStatic.getJsonArray(url: String): Array<Json> {
+    val channel = Channel<Array<Json>>()
+    getJSON(url = url) { _, _, jqXHR ->
+        channel.offer(JSON.parse(jqXHR.responseText))
+    }
+    return channel.receive()
+}
+
+suspend fun JQueryStatic.getJson(url: String): Json {
     val channel = Channel<Json>()
     getJSON(url = url) { _, _, jqXHR ->
         channel.offer(jqXHR.json)
+    }
+    return channel.receive()
+}
+
+suspend fun JQueryStatic.getJsonText(url: String): String {
+    val channel = Channel<String>()
+    getJSON(url = url) { _, _, jqXHR ->
+        channel.offer(jqXHR.responseText)
     }
     return channel.receive()
 }
