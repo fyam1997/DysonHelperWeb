b5562ca16dbdcd6bcae517f7904d64f2bb8e3c5d
Fyam <fung19971115@gmail.com>
2021-02-13 21:07:02 +0800

    - Update itemDetailView

---
+++ src/main/kotlin/flow/mainscreen/components/ItemDetail.kt
@@ -0,0 +1,37 @@
+package flow.mainscreen.components
+
+import kotlinx.css.*
+import kotlinx.html.*
+import model.ItemDetailModel
+import utils.css
+import utils.size
+
+fun HtmlBlockTag.itemDetailView(detail: ItemDetailModel) {
+    div {
+        style = css {
+            display = Display.flex
+            flexDirection = FlexDirection.column
+        }
+        itemDesc(detail)
+    }
+}
+
+fun HtmlBlockTag.itemDesc(detail: ItemDetailModel) {
+    div {
+        style = css {
+            display = Display.flex
+            flexDirection = FlexDirection.row
+            alignItems = Align.center
+        }
+        img {
+            style = css { size = 32.px }
+            src = detail.item.iconPath
+        }
+        p {
+            b { +detail.item.name }
+            br()
+            if (detail.item.desc.isNotEmpty())
+                +detail.item.desc
+        }
+    }
+}
