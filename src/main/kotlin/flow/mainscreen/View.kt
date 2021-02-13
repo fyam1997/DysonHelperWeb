a9699640c51ff896c128859d3141ba030a8c0508
Fyam <fung19971115@gmail.com>
2021-02-13 19:43:47 +0800

    - Update view architect

--- src/main/kotlin/flow/mainscreen/View.kt
+++ src/main/kotlin/flow/mainscreen/View.kt
@@ -4,11 +4,8 @@ import flow.mainscreen.components.recipeList
 import kotlinx.css.*
 import kotlinx.html.*
 import kotlinx.html.dom.append
-import model.Recipe
 import org.w3c.dom.Document
-import utils.collectWithScope
-import utils.css
-import utils.size
+import utils.*
 
 class View constructor(
     private val document: Document,
@@ -49,36 +46,48 @@ class View constructor(
         }
     }
 
-    private fun TagConsumer<*>.recipeListColumn() {
+    private fun HtmlBlockTag.recipeListColumn() {
         div {
             style = css {
                 display = Display.flex
                 flexDirection = FlexDirection.column
             }
             div {
-                id = "detailView"
                 style = css {
-                    flex(flexGrow = 0.0, flexShrink = 0.0, flexBasis = FlexBasis.auto)
-                    size = 200.px
-                    background = Color.aliceBlue.value
+                    wrapContent()
+                    minHeight = 200.px
                     overflow = Overflow.auto
                 }
+                itemDetailCell()
             }
-            recipeListCell()
+            div {
+                style = css {
+                    fillRemaining()
+                    minHeight = 200.px
+                    overflow = Overflow.auto
+                }
+                recipeListCell()
+            }
+
         }
     }
 
-    private fun TagConsumer<*>.recipeListCell() {
+    private fun HtmlBlockTag.itemDetailCell() {
         div {
-            id = "recipeList"
-            style = css {
-                flex(flexGrow = 1.0, flexShrink = 1.0, flexBasis = FlexBasis.auto)
-                minHeight = 50.pct
-                overflow = Overflow.auto
-            }
+            id = "itemDetail"
         }
+        div {
+            id = "canBeInputList"
+        }
+        div {
+            id = "canBeOutPutList"
+        }
+    }
+
+    private fun HtmlBlockTag.recipeListCell() {
+        id = "recipeList"
         vm.recipes.collectWithScope {
-            document.getElementById("recipeList")?.apply {
+            element("recipeList")?.apply {
                 innerHTML = ""
                 append {
                     recipeList(list = it, iconMap = vm.iconMap.value, onItemClick = vm::onItemClick)
@@ -88,7 +97,7 @@ class View constructor(
     }
 
     private fun changeIcon(iconSrc: String) {
-        document.getElementById("favicon")?.let {
+        element("favicon")?.let {
             document.head?.removeChild(it)
         }
         document.head?.append?.link {
