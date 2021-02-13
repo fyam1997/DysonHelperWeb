6a9ce7b8e74e16e486ba9462e06efa0975bb0172
Fyam <fung19971115@gmail.com>
2021-02-13 10:29:23 +0800

    - Update css ext

--- src/main/kotlin/utils/CssExt.kt
+++ src/main/kotlin/utils/CssExt.kt
@@ -1,8 +1,18 @@
 package utils
 
 import kotlinx.css.CSSBuilder
+import kotlinx.css.LinearDimension.Companion.none
+import kotlinx.css.height
+import kotlinx.css.width
 
 fun css(
     builder: CSSBuilder = CSSBuilder(),
     script: CSSBuilder.() -> Unit
 ) = builder.apply(script).toString()
+
+var CSSBuilder.size
+    get() = if (width != height) none else width
+    set(value) {
+        width = value
+        height = value
+    }
