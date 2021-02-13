aa6b54d8aa6f4ab9e6f05f8cddb4fbf2b45b2fd0
Fyam <fung19971115@gmail.com>
2021-02-13 21:02:47 +0800

    - Update iconMap

--- src/main/kotlin/flow/mainscreen/ViewModel.kt
+++ src/main/kotlin/flow/mainscreen/ViewModel.kt
@@ -13,13 +13,18 @@ import utils.toMap
 
 class ViewModel {
     var recipes = MutableStateFlow(emptyList<Recipe>())
-    var iconMap = MutableStateFlow(emptyMap<String, String>())
 
     var focusingItem = MutableStateFlow<ItemDetailModel?>(null)
 
     fun initData() {
         GlobalScope.launch {
-            iconMap.value = JQueryStatic.getJson(url = "data/iconMap.json").toMap<String>()
+            val iconMap = JQueryStatic.getJson(url = "data/iconMap.json").toMap<String>()
+            fun makeItem(id: String) = Item(
+                id = id,
+                name = id,
+                desc = "",
+                iconPath = "itemIcons/${iconMap[id].orEmpty()}"
+            )
             recipes.value = JQueryStatic.getJsonArray("data/recipe.json").map { json ->
                 Recipe(
                     outputs = json.getJson("outputs").toMap<Int>().map {
@@ -49,6 +54,4 @@ class ViewModel {
         focusingItem.value = ItemDetailModel(item, canBeInputListTemp, canBeOutputListTemp)
     }
 
-    private fun makeItem(id: String) =
-        Item(id = id, name = id, desc = "", iconPath = "itemIcons/${iconMap.value[id].orEmpty()}")
 }
