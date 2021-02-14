867baf172b6da850514ee384bc886be0c2bcd178
Fyam <fung19971115@gmail.com>
2021-02-15 01:30:12 +0800

    - Update copy

--- src/main/kotlin/utils/util.kt
+++ src/main/kotlin/utils/util.kt
@@ -4,3 +4,5 @@ fun <T> T.doIf(
     condition: Boolean,
     action: T.() -> T
 ): T = if (condition) action() else this
+
+fun <K, V> Map<K, V>.copy(action: MutableMap<K, V>.() -> Unit) = toMutableMap().apply(action)
