c92f080abbe50ff0d33b5fafd24380505330811b
Fyam <fung19971115@gmail.com>
2021-02-14 11:26:21 +0800

    - Update vm / recipe factory

--- src/main/kotlin/model/Recipe.kt
+++ src/main/kotlin/model/Recipe.kt
@@ -1,8 +1,18 @@
 package model
 
+import kotlinx.serialization.Serializable
+
 data class Recipe(
     val outputs: Map<Item, Int>,
     val inputs: Map<Item, Int>,
     val time: Int,
     val facility: Item
-)
+){
+    @Serializable
+    data class Raw(
+        val outputs: Map<String, Int>,
+        val inputs: Map<String, Int>,
+        val time: Int,
+        val facility: String
+    )
+}
