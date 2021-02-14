b9ad27b24d889bdb8c395581b468daf7c635193d
Fyam <fung19971115@gmail.com>
2021-02-14 22:13:30 +0800

    - Update list view

--- src/main/kotlin/components/RecipeList.kt
+++ src/main/kotlin/components/RecipeList.kt
@@ -1,71 +1,86 @@
 package components
 
 import kotlinx.css.*
-import kotlinx.html.*
+import kotlinx.html.HtmlTagMarker
 import kotlinx.html.js.onClickFunction
+import kotlinx.html.title
 import model.Item
 import model.Recipe
-import utils.css
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.td
+import styled.*
 import utils.size
 
-fun TagConsumer<*>.recipeListView(
-    list: List<Recipe>,
-    onItemClick: (Item) -> Unit
-) {
-    table("unSelectable") {
-        style = css {
-            borderCollapse = BorderCollapse.collapse
-        }
-        list.forEach { recipe ->
-            recipeRowView(recipe, onItemClick)
-        }
-    }
-}
-
-fun TagConsumer<*>.recipeRowView(
-    recipe: Recipe,
-    onItemClick: (Item) -> Unit
-) {
-    tr("hoverRow") {
-        style = css {
-            borderBottomStyle = BorderStyle.solid
-            borderBottomColor = Color.darkGrey
-            borderBottomWidth = 1.px
-        }
-        itemCellView(recipe.outputs, onItemClick)
-        td { +"←" }
-        itemCellView(recipe.inputs, onItemClick)
-        // TODO check language here
-        td { +recipe.facility.name }
-        td { +recipe.time.toString() }
-    }
-}
-
-@HtmlTagMarker
-private fun TagConsumer<*>.itemCellView(
-    items: Map<Item, Int>,
-    onItemClick: (Item) -> Unit
-) {
-    td {
-        div {
-            style = css {
-                height = LinearDimension.auto
-                display = Display.grid
-                gridTemplateColumns = GridTemplateColumns("auto auto auto")
-                alignItems = Align.center
-                justifyContent = JustifyContent.center
+class RecipeList : RComponent<RecipeList.Props, RState>() {
+    override fun RBuilder.render() {
+        styledTable {
+            css {
+                borderCollapse = BorderCollapse.collapse
             }
-            items.forEach {
-                val item = it.key
-                img {
-                    style = css { size = 32.px }
-                    src = item.iconPath
-                    // TODO check language here
-                    alt = item.name
-                    title = item.name
-                    onClickFunction = { onItemClick(item) }
+            props.list.forEach { recipe ->
+                recipeRowView(recipe, props.onItemClick)
+            }
+        }
+    }
+
+    private fun RBuilder.recipeRowView(
+        recipe: Recipe,
+        onItemClick: (Item) -> Unit
+    ) {
+        styledTr {
+            css {
+                borderBottomStyle = BorderStyle.solid
+                borderBottomColor = Color.darkGrey
+                borderBottomWidth = 1.px
+                hover {
+                    background = Color.aliceBlue.value
+                }
+            }
+            itemCellView(recipe.outputs, onItemClick)
+            td { +"←" }
+            itemCellView(recipe.inputs, onItemClick)
+            // TODO check language here
+            td { +recipe.facility.name }
+            td { +recipe.time.toString() }
+        }
+    }
+
+    @HtmlTagMarker
+    private fun RBuilder.itemCellView(
+        items: Map<Item, Int>,
+        onItemClick: (Item) -> Unit
+    ) {
+        styledTd {
+            styledDiv {
+                css {
+                    height = LinearDimension.auto
+                    display = Display.grid
+                    gridTemplateColumns = GridTemplateColumns("auto auto auto")
+                    alignItems = Align.center
+                    justifyContent = JustifyContent.center
+                }
+                items.forEach {
+                    val item = it.key
+                    styledImg {
+                        css { size = 32.px }
+                        attrs {
+                            src = item.iconPath
+                            // TODO check language here
+                            alt = item.name
+                            title = item.name
+                            onClickFunction = { onItemClick(item) }
+                        }
+                    }
                 }
             }
         }
+    }
+
+    interface Props : RProps {
+        var list: List<Recipe>
+        var onItemClick: (Item) -> Unit
     }
 }
