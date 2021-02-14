c4e3a97867115499b95d2619a50b6b1a43dee0ab
Fyam <fung19971115@gmail.com>
2021-02-14 11:26:18 +0800

    - Update vm / recipe factory

--- src/main/kotlin/flow/mainscreen/ViewModel.kt
+++ src/main/kotlin/flow/mainscreen/ViewModel.kt
@@ -1,44 +1,24 @@
 package flow.mainscreen
 
-import kotlinx.browser.window
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import model.Item
 import model.ItemDetailModel
 import model.Recipe
-import utils.fetchJson
-import utils.fetchJsonArray
-import utils.getJson
-import utils.toMap
 
-class ViewModel {
+class ViewModel(
+    private val jsonRepo: JsonRepository
+) {
     var recipes = MutableStateFlow(emptyList<Recipe>())
 
     var focusingItem = MutableStateFlow<ItemDetailModel?>(null)
 
     fun initData() {
         GlobalScope.launch {
-            val iconMap = window.fetchJson("data/iconMap.json").toMap<String>()
-            fun makeItem(id: String) = Item(
-                id = id,
-                name = id,
-                desc = "",
-                iconPath = "itemIcons/${iconMap[id].orEmpty()}"
-            )
-
-            recipes.value = window.fetchJsonArray("data/recipe.json").map { json ->
-                Recipe(
-                    outputs = json.getJson("outputs").toMap<Int>().map {
-                        makeItem(it.key) to it.value
-                    }.toMap(),
-                    inputs = json.getJson("inputs").toMap<Int>().map {
-                        makeItem(it.key) to it.value
-                    }.toMap(),
-                    time = json["time"].unsafeCast<Int>(),
-                    facility = makeItem(json["facility"].unsafeCast<String>())
-                )
-            }
+            val iconMap = jsonRepo.iconMap()
+            val factory = RecipeFactory(iconMap)
+            recipes.value = jsonRepo.recipes().map(factory::makeRecipe)
         }
     }
 
@@ -56,4 +36,27 @@ class ViewModel {
         focusingItem.value = ItemDetailModel(item, canBeInputListTemp, canBeOutputListTemp)
     }
 
+    class RecipeFactory(
+        private val iconMap: Map<String, String>
+    ) {
+        fun makeRecipe(raw: Recipe.Raw) = with(raw) {
+            Recipe(
+                outputs = outputs.map {
+                    makeItem(it.key) to it.value
+                }.toMap(),
+                inputs = inputs.map {
+                    makeItem(it.key) to it.value
+                }.toMap(),
+                time = time,
+                facility = makeItem(facility)
+            )
+        }
+
+        private fun makeItem(id: String) = Item(
+            id = id,
+            name = id,
+            desc = "",
+            iconPath = "itemIcons/${iconMap[id].orEmpty()}"
+        )
+    }
 }
