01f8abe613e2b880f4b4734ef50406457c353330
Fyam <fung19971115@gmail.com>
2021-03-07 16:39:52 +0800

    - Update mapKeys

--- src/main/kotlin/flow/mainscreen/RecipeFactory.kt
+++ src/main/kotlin/flow/mainscreen/RecipeFactory.kt
@@ -8,12 +8,8 @@ class RecipeFactory(
 ) {
     fun makeRecipe(raw: Recipe.Raw) = with(raw) {
         Recipe(
-            outputs = outputs.map {
-                makeItem(it.key) to it.value
-            }.toMap(),
-            inputs = inputs.map {
-                makeItem(it.key) to it.value
-            }.toMap(),
+            outputs = outputs.mapKeys { makeItem(it.key) }.toMap(),
+            inputs = inputs.mapKeys { makeItem(it.key) }.toMap(),
             time = time,
             facility = makeItem(facility)
         )
