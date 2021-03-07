f7207c833ef8fb2adb515932a1fcf1116ad7f8bb
Fyam <fung19971115@gmail.com>
2021-03-07 17:09:06 +0800

    - Update CacheRepository

--- src/main/kotlin/flow/mainscreen/CacheRepository.kt
+++ src/main/kotlin/flow/mainscreen/CacheRepository.kt
@@ -1,16 +1,36 @@
 package flow.mainscreen
 
 import kotlinx.browser.localStorage
+import kotlinx.serialization.Serializable
 import model.Recipe
 import utils.decodeToJson
 import utils.encodeToString
 
 class CacheRepository {
     suspend fun putSelectedRecipeMap(map: Map<Recipe.Raw, Int>) {
-        localStorage.setItem("selectedRecipeMap", map.encodeToString())
+        val rawString = map.toStorageObject().encodeToString()
+        localStorage.setItem("selectedRecipeMap", rawString)
     }
 
     suspend fun getSelectedRecipeMap(): Map<Recipe.Raw, Int>? {
-        return localStorage.getItem("selectedRecipeMap")?.decodeToJson()
+        val rawString = localStorage.getItem("selectedRecipeMap")
+        val list: List<SelectedRecipe>? = rawString?.decodeToJson()
+        return list?.map {
+            it.recipe to it.number
+        }?.toMap()
+    }
+
+    //TODO use this class for view logic
+    @Serializable
+    data class SelectedRecipe(
+        val recipe: Recipe.Raw,
+        val number: Int
+    )
+
+    private fun Map<Recipe.Raw, Int>.toStorageObject(): List<SelectedRecipe> = map {
+        SelectedRecipe(
+            recipe = it.key,
+            number = 0
+        )
     }
 }
