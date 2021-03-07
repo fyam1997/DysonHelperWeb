17d0b8df14c7b649032c6d1fe2e3a9c19399d44e
Fyam <fung19971115@gmail.com>
2021-03-07 16:53:12 +0800

    - Update CacheRepository

--- src/main/kotlin/flow/mainscreen/CacheRepository.kt
+++ src/main/kotlin/flow/mainscreen/CacheRepository.kt
@@ -1,8 +1,16 @@
 package flow.mainscreen
 
+import kotlinx.browser.localStorage
 import model.Recipe
+import utils.decodeToJson
+import utils.encodeToString
 
 class CacheRepository {
-    suspend fun putSelectedRecipeMap(map: Map<Recipe, Int>) {
+    suspend fun putSelectedRecipeMap(map: Map<Recipe.Raw, Int>) {
+        localStorage.setItem("selectedRecipeMap", map.encodeToString())
+    }
+
+    suspend fun getSelectedRecipeMap(): Map<Recipe.Raw, Int>? {
+        return localStorage.getItem("selectedRecipeMap")?.decodeToJson()
     }
 }
